c187960563f6c6968bb1a6af81b929a4ef749ff9
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-07 19:35:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  arraynew = []
- array.each do |sqnum|
-   arraynew << sqnum.sqrt
- end
- arraynew
+  square_array = []
+   array.each do |sqnum|
+     square_array.push(sqnum *sqnum)
+   end
+   square_array
 end
+
