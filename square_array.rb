b49385c182f0c709ada7ec4aff0920f51208ce8a
Daniel Rooker <dannyrooks@gmail.com>
2019-10-08 07:01:43 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
 def square_array(num)
   new_array = []
-  num.each do |x| 
-    squared_num = x ** 2
-    new_array << squared_num
-  end
+  num.each { |x| new_array << x ** 2 }
     new_array
 end
