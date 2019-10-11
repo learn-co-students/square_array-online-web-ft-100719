203adc344a1d553546268415e194a507eb58cd7c
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-11 05:48:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def 
+  square_array = [2,3,4]
+  number_i = [2,3,4]
+  number_f = [4,9,16]
+  number_i.each ** number_i.each == |number_f|.size
+  
 end
